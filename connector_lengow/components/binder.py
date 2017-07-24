d45405c54af2f7c51602bfee2e9d2929c63e3657
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-07-24 17:53:09 +0200

    [FIX] add specific internal binding method for lengow binder

--- connector_lengow/components/binder.py
+++ connector_lengow/components/binder.py
@@ -4,6 +4,7 @@
 
 
 from odoo.addons.component.core import Component
+from odoo import tools
 
 
 class LengowModelBinder(Component):
@@ -18,3 +19,35 @@ class LengowModelBinder(Component):
     ]
 
     _external_field = 'lengow_id'
+
+    def to_internal(self, external_id, unwrap=False):
+        """ Give the Odoo recordset for an external ID
+
+        :param external_id: external ID for which we want
+                            the Odoo ID
+        :param unwrap: if True, returns the normal record
+                       else return the binding record
+        :return: a recordset, depending on the value of unwrap,
+                 or an empty recordset if the external_id is not mapped
+        :rtype: recordset
+        """
+        bindings = self.model.with_context(active_test=False).search(
+            [(self._external_field, '=', tools.ustr(external_id)),
+             (self._backend_field, '=', self.backend_record.id)]
+        )
+        if not bindings:
+            if unwrap:
+                return self.model.browse()[self._odoo_field]
+            return self.model.browse()
+
+        if len(bindings) > 1:
+            # can be the case for lengow.product.product because the same
+            # product can be binded to several catalogue
+            assert len(set([binding[self._odoo_field]
+                            for binding in bindings])) == 1, (
+                "Multiple value for same id %s" % external_id)
+            bindings = bindings[0]
+
+        if unwrap:
+            bindings = bindings[self._odoo_field]
+        return bindings
